dcea43da2db1ed0115a2fbf974141c644129a007
root <root@rhel81a.sudo.net>
2020-02-14 16:54:45 +0100

    added firewalld

--- ansible/scripts/rhelhost-prep.sh
+++ ansible/scripts/rhelhost-prep.sh
@@ -8,7 +8,7 @@ echo "rhel ALL=(ALL) NOPASSWD: ALL" > /etc/sudoers.d/rhel
 chmod 0440 /etc/sudoers.d/rhel
 
 #install enable and open firewall for cockpit
-yum install cockpit-composer cockpit bash-completion -y
+yum install firewalld cockpit-composer cockpit bash-completion -y
 systemctl enable --now cockpit.socket
 firewall-cmd --add-service=cockpit
 firewall-cmd --add-service=cockpit --permanent
