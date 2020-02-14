19f799471e6acad9e68c21e429ba61467172605c
Håkan Hagenrud <hakan@redhat.com>
2020-02-14 15:20:15 +0100

    now powershell script runs

--- ansible/scripts/winhost-prep.ps1
+++ ansible/scripts/winhost-prep.ps1
@@ -5,5 +5,5 @@ $ansfile = "c:\ConfigureRemotingForAnsible.ps1"
 powershell.exe -ExecutionPolicy ByPass -File $ansfile
 $myPassword = "Password1"
 $Secure_String_Pwd = ConvertTo-SecureString $myPassword -AsPlainText -Force
-PS C:\> New-LocalUser "wsadder" -Password $Secure_String_Pwd -FullName "Workstation Adder" -Description "The extra account for managing the server."
+New-LocalUser "wsadder" -Password $Secure_String_Pwd -FullName "Workstation Adder" -Description "The extra account for managing the server."
 Add-LocalGroupMember -Group Administrators -Member wsadder
